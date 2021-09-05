9078947037f3224b47132adc2e973d274c6b49a6
Ricardo-we <totigamer75@gmail.com>
2021-09-04 21:54:48 -0600

    no desc

--- ProyectoEspecialPro/urls.py
+++ ProyectoEspecialPro/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 import  ProyectoEspecialApp 
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,7 @@ urlpatterns = [
     path('fotos/', include('fotos.urls')),
     path('audios/', include('audios.urls')),
     path('videos/', include('videos.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {"document_root":settings.MEDIA_ROOT})
 ]
 
 if settings.DEBUG:
